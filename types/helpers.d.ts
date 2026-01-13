fa01dd19429df0a7c26eacbe9304db2df0efe0d2
张晨 <giszhc@163.com>
2026-01-13 14:21:37 +0800

    refactor: 删除旧类型定义文件并更新gitignore

--- types/helpers.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * 坐标转文本
- * 支持 XY / XYZ / XYM / XYZM
- */
-export declare function coordToText(coord: number[]): string;
--- types/index.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Feature, GeoJSON, Geometry } from "./types.d.ts";
-export declare const geometryToWKT: (geometry: Geometry) => string;
-export declare const featureToWKT: (input: Feature) => string;
-export declare const featureCollectionToWKT: (input: GeoJSON) => string;
-export declare const toWKT: (input: GeoJSON) => string;
